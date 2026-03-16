a0d9d03a5738fa9d5cd1c058ed9846cf9a6bb610
MD RATUL HASSAN SHUVO <ratulhasan01715@gmail.com>
2026-03-16 17:31:00 +0600

    Add files via upload

---
+++ Calculator_project_By_C.c
@@ -0,0 +1,57 @@
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+
+  char oparetor;
+  double num1;
+  double num2;
+  double result;
+  printf("ENTER ANY OPARETOR:->");
+  scanf("%C", &oparetor);
+
+  printf("Enter number1 to calculate= ");
+  scanf("%lf", &num1);
+  printf("Enter number2 to calculate= ");
+  scanf("%lf", &num2);
+  switch (oparetor)
+  {
+  case '^':
+    result = pow(num1, num2);
+    printf("The result(for power)=%lf", result);
+    break;
+  case '~':
+    result = sqrt(num1);
+    printf("\nThe value of root=%lf", result);
+    break;
+  case '+':
+    result = num1 + num2;
+    printf("The result=%lf\n", result);
+    break;
+  case '-':
+    result = num1 - num2;
+    printf("The result%lf", result);
+    break;
+  case '*':
+    result = num1 * num2;
+    printf("The result =%lf", result);
+    break;
+  case '/':
+    if (num2 == 0)
+    {
+      printf("Syntax Error");
+    }
+    else
+    {
+      result = num1 / num2;
+      printf("The result=%lf", result);
+    }
+    break;
+  default:
+    printf("\n%c Is not a valued operator ", oparetor);
+  }
+
+  return 0;
+}
